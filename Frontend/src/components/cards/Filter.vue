e4218ab6324549337d7b6aa173a8c79afe715da4
Philippe <ph.waeber@gmail.com>
2020-03-03 15:08:48 +0100

    Reworked filter - Create a filter button component and replace all bootstrap buttons by it

--- Frontend/src/components/cards/Filter.vue
+++ Frontend/src/components/cards/Filter.vue
@@ -1,198 +1,101 @@
 <template>
   <div id="user-cards">
     <div class="filterContainer row no-gutters">
+      <!-- Rarities -->
       <div class="col-sm-3 col-md-auto m-1">
-        <b-button
-          :pressed.sync="filter.rarity.common"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/theme-stones/common/' + cardIdentifiers.themeNeutralId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.rarity.rare"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/theme-stones/special/' + cardIdentifiers.rarityRareId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.rarity.epic"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/theme-stones/special/' + cardIdentifiers.rarityEpicId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.rarity.legendary"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/theme-stones/special/' + cardIdentifiers.rarityLegendaryId + '.png'"
-          />
-        </b-button>
+        <filter-button
+          :isActive="filter.rarity.common"
+          :imgSrc="'/img/cards/theme-stones/common/' + cardIdentifiers.themeNeutralId + '.png'"
+          v-on:isActive="filter.rarity.common = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.rarity.rare"
+          :imgSrc="'/img/cards/theme-stones/special/' + cardIdentifiers.rarityRareId + '.png'"
+          v-on:isActive="filter.rarity.rare = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.rarity.epic"
+          :imgSrc="'/img/cards/theme-stones/special/' + cardIdentifiers.rarityEpicId + '.png'"
+          v-on:isActive="filter.rarity.epic = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.rarity.legendary"
+          :imgSrc="'/img/cards/theme-stones/special/' + cardIdentifiers.rarityLegendaryId + '.png'"
+          v-on:isActive="filter.rarity.legendary = $event; emitFilterChanged()"
+        />
       </div>
+
+      <!-- Themes -->
       <div class="col-sm-3 col-md-auto m-1">
-        <b-button
-          :pressed.sync="filter.theme.adventure"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/theme-icons/' + cardIdentifiers.themeAdventureId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.theme.fantasy"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/theme-icons/' + cardIdentifiers.themeFantasyId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.theme.scifi"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/theme-icons/' + cardIdentifiers.themeScifiId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.theme.mystical"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/theme-icons/' + cardIdentifiers.themeMysticalId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.theme.superhero"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/theme-icons/' + cardIdentifiers.themeSuperheroId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.theme.neutral"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/theme-icons/' + cardIdentifiers.themeNeutralId + '.png'"
-          />
-        </b-button>
+        <filter-button
+          :isActive="filter.theme.adventure"
+          :imgSrc="'/img/cards/theme-icons/' + cardIdentifiers.themeAdventureId + '.png'"
+          v-on:isActive="filter.theme.adventure = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.theme.fantasy"
+          :imgSrc="'/img/cards/theme-icons/' + cardIdentifiers.themeFantasyId + '.png'"
+          v-on:isActive="filter.theme.fantasy = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.theme.scifi"
+          :imgSrc="'/img/cards/theme-icons/' + cardIdentifiers.themeScifiId + '.png'"
+          v-on:isActive="filter.theme.scifi = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.theme.mystical"
+          :imgSrc="'/img/cards/theme-icons/' + cardIdentifiers.themeMysticalId + '.png'"
+          v-on:isActive="filter.theme.mystical = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.theme.superhero"
+          :imgSrc="'/img/cards/theme-icons/' + cardIdentifiers.themeSuperheroId + '.png'"
+          v-on:isActive="filter.theme.superhero = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.theme.neutral"
+          :imgSrc="'/img/cards/theme-icons/' + cardIdentifiers.themeNeutralId + '.png'"
+          v-on:isActive="filter.theme.neutral = $event; emitFilterChanged()"
+        />
       </div>
+
+      <!-- Types -->
       <div class="col-sm-3 col-md-auto m-1">
-        <b-button
-          :pressed.sync="filter.type.tank"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/type-icons/' + cardIdentifiers.characterTypeTankId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.type.melee"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/type-icons/' + cardIdentifiers.characterTypeMeleeId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.type.assassin"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/type-icons/' + cardIdentifiers.characterTypeAssassinId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.type.ranged"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/type-icons/' + cardIdentifiers.characterTypeRangedId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.type.totem"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/type-icons/' + cardIdentifiers.characterTypeTotemId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.type.spell"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/type-icons/' + cardIdentifiers.cardTypeSpellId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
-          />
-        </b-button>
-        <b-button
-          :pressed.sync="filter.type.trap"
-          @click="emitFilterChanged"
-          variant="outline-dark"
-          shadow-none
-        >
-          <img
-            height="24px"
-            :src="'/img/cards/type-icons/' + cardIdentifiers.cardTypeTrapId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
-          />
-        </b-button>
+        <filter-button
+          :isActive="filter.type.tank"
+          :imgSrc="'/img/cards/type-icons/' + cardIdentifiers.characterTypeTankId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
+          v-on:isActive="filter.type.tank = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.type.melee"
+          :imgSrc="'/img/cards/type-icons/' + cardIdentifiers.characterTypeMeleeId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
+          v-on:isActive="filter.type.melee = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.type.assassin"
+          :imgSrc="'/img/cards/type-icons/' + cardIdentifiers.characterTypeAssassinId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
+          v-on:isActive="filter.type.assassin = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.type.ranged"
+          :imgSrc="'/img/cards/type-icons/' + cardIdentifiers.characterTypeRangedId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
+          v-on:isActive="filter.type.ranged = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.type.totem"
+          :imgSrc="'/img/cards/type-icons/' + cardIdentifiers.characterTypeTotemId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
+          v-on:isActive="filter.type.totem = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.type.spell"
+          :imgSrc="'/img/cards/type-icons/' + cardIdentifiers.cardTypeSpellId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
+          v-on:isActive="filter.type.spell = $event; emitFilterChanged()"
+        />
+        <filter-button
+          :isActive="filter.type.trap"
+          :imgSrc="'/img/cards/type-icons/' + cardIdentifiers.cardTypeTrapId + '/special/' + cardIdentifiers.rarityEpicId + '.png'"
+          v-on:isActive="filter.type.trap = $event; emitFilterChanged()"
+        />
       </div>
       <div class="col-sm-3 col-md-auto m-1">
         <b-button @click="resetFilter" variant="outline-dark" shadow-none>All</b-button>
@@ -207,9 +110,14 @@ import Vue from 'vue'
 
 // @ts-ignore
 import { cardIdentifiers } from '@/_constants'
+// @ts-ignore
+import FilterButton from './FilterButton'
 
 export default Vue.extend({
   name: 'CardsFilterComponent',
+  components: {
+    FilterButton
+  },
   data: function() {
     return {
       filter: {
@@ -276,6 +184,10 @@ export default Vue.extend({
 
       this.emitFilterChanged()
     },
+    updateFilter(filterProperty: boolean, isActive: boolean) {
+      filterProperty = isActive
+      this.emitFilterChanged()
+    },
     emitFilterChanged() {
       this.$emit('filter', { ...this.filter })
     }
@@ -283,7 +195,7 @@ export default Vue.extend({
 })
 </script>
 
-<style>
+<style scoped>
 .filterContainer {
   background: lightgray;
 }
---
+++ Frontend/src/components/cards/FilterButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <img
+    :class="['bt-filter', 'border border-dark', 'rounded', 'p-1', isActive ? 'bt-filter-enabled' : 'bt-filter-disabled']"
+    height="36px"
+    :src="imgSrc"
+    @click="emitToggleIsActive()"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'FilterButtonComponent',
+  props: {
+    isActive: Boolean,
+    imgSrc: String
+  },
+  methods: {
+    emitToggleIsActive() {
+      this.$emit('isActive', !this.isActive)
+    }
+  }
+})
+</script>
+
+<style scoped>
+.bt-filter {
+  cursor: pointer;
+}
+.bt-filter-enabled {
+  background: rgb(33, 37, 41);
+}
+.bt-filter-disabled {
+  background: transparent;
+}
+</style>
